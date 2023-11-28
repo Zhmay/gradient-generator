4bb2bea39a3284e3facaf4859cc92616976548de
bogdan <bogdanzhmay@gmail.com>
2023-11-28 23:51:21 +0200

    finalized widget

--- main.js
+++ main.js
@@ -1,27 +1,53 @@
-let colorOne = document.getElementById('color-a');
-let colorTwo = document.getElementById('color-b');
+const inputColor = document.querySelectorAll('input[type="color"]');
+const colorOne = document.getElementById('color-a');
+const colorTwo = document.getElementById('color-b');
+const outputCode = document.getElementById('code');
+const copyOverlay = document.getElementById('copy-overlay');
 let currentDirection = 'to bottom';
-let outputCode = document.getElementById('code');
 
 function setDirection(value, _this) {
-  let directions = document.querySelectorAll('.buttons button');
+  const directions = document.querySelectorAll('.buttons button');
 
   for(let i of directions) {
     i.classList.remove('active');
   }
   _this.classList.add('active');
   currentDirection = value;
+
+  generateCode();
 }
 
-function copyText() {
+function copyText(_this) {
   outputCode.select();
   document.execCommand('copy');
-  alert('Gradient Copied!');
+
+  copyOverlay.style.transition = 'all 250ms ease-in-out';
+  copyOverlay.style.borderRadius = '50%';
+  copyOverlay.style.transform = 'translateY(0) scaleX(0.07) scaleY(0.5)';
+  setTimeout(function() {
+    copyOverlay.style.opacity = 0;
+  }, 200),
+  setTimeout(function() {
+    copyOverlay.style.transition = 'all 1ms linear';
+    copyOverlay.style.transform = '';
+    copyOverlay.style.borderRadius = 0
+  }, 500),
+  setTimeout(function() {
+    copyOverlay.style.opacity = 1
+    copyOverlay.style.transition = 'all 150ms ease-in-out';
+  }, 600)
 }
 
+inputColor.forEach(element => {
+  element.addEventListener('change', () => {
+    generateCode();
+  })
+});
+
 function generateCode() {
-  outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
-  document.getElementsByTagName('body')[0].style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+  outputCode.value = `background-image: linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value});`;
+  copyOverlay.style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
+  document.querySelector('body').style.backgroundImage = `linear-gradient(${currentDirection}, ${colorOne.value}, ${colorTwo.value})`;
 }
 
 generateCode();
